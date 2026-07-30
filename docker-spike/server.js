4adbaf2ea2fbeb4012533bb3b0f32f3060c7242f
Aryan Cyrus <aryan.m10@yahoo.com>
2026-07-30 20:15:08 +1000

    Finished docker tutotial

--- docker-spike/server.js
+++ docker-spike/server.js
@@ -1,26 +1,20 @@
-import express from "express";
+const express = require("express");
 const PORT = process.env.PORT || 3000;
-
 
 const server = express();
 
-
 server.get("/data", (req, res) => {
-  console.log("data has been requested")
+  console.log("data has been requested");
   res.status(204).send({
     success: true,
-    data: [
-      1, 1, 1, 1, 1
-    ]
-  })
-})
-
+    data: [1, 1, 1, 1, 1]
+  });
+});
 
 server.get("/", (req, res) => {
-  console.log("data has been requested")
+  console.log("data has been requested");
   res.send("welcome to my epic server");
-})
-
+});
 
 server.listen(PORT, () => {
   console.log(`Server is running locally at ${PORT}`);
